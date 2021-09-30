1b8e794d26e976025b12cd5deb0690c10fc39e51
davidrb92 <davidrb1992@hotmail.com>
2021-09-30 15:45:03 +0200

    terminado2

---
+++ app/code/Hiberus/HiberusRebollar/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+function ocultarNotas(){
+    alert("EEE")
+    var x= document.getElementById("notas");
+
+    if(x.style.display==="none"){
+        x.style.display = "block"
+    }else{
+        x.style.display = "none"
+    }
+}
